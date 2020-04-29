b6fc04a9982478e167c65afdc493607b5dd8aee8
Arseniy <tyletsky@mail.ru>
2020-04-29 16:25:31 +0300

    Only relevant info is serializing to json now

--- restful_API/Models/User.cs
+++ restful_API/Models/User.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 using restful_API.Models.Interfaces;
 
 namespace restful_API.Models
 {
+    [DataContract]
     public class User : IUser
     {
+        [DataMember]
         public string Login { get; set; }
+        [DataMember]
         public string Password { get; set; }
 
         public virtual IList<UserWeatherForecast> UserWeatherForecasts { get; set; }
--- restful_API/Models/WeatherForecast.cs
+++ restful_API/Models/WeatherForecast.cs
@@ -1,15 +1,22 @@
 using restful_API.Models.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace restful_API.Models
 {
+    [DataContract]
     public class WeatherForecast : IWeatherForecast
     {
+        [DataMember]
         public Guid Id { get; set; }
+        [DataMember]
         public DateTime Date { get; set; }
+        [DataMember]
         public string City { get; set; }
+        [DataMember]
         public float Temperature { get; set; }
+        [DataMember]
         public string Summary { get; set; }
 
 
